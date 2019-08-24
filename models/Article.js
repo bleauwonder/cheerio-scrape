7d1300b8559039c576e57ffc2ce982cb11c56797
bleauwonder <elyzableau.om@gmail.com>
2019-08-24 12:56:28 -0400

    cleaned up code

--- models/Article.js
+++ models/Article.js
@@ -1,17 +1,14 @@
 var mongoose = require("mongoose");
 
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 var ArticleSchema = new Schema({
   // image
   image: {
     type: String,
     required: true
   },
-  // `title` is required and of type String
+
   title: {
     type: String,
     required: true
@@ -21,19 +18,13 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
-  // `link` is required and of type String
+
   link: {
     type: String,
     required: true
   }
-  // comment: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: "Comment"
-  // }
 });
 
-// This creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
 
-// Export the Article model
 module.exports = Article;
--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
+// Scraping tools
 var axios = require("axios");
 var cheerio = require("cheerio");
 
@@ -39,8 +37,6 @@ require("./htmlRoutes")(app);
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
-// mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
 
 // Routes
 
@@ -83,12 +79,10 @@ app.get("/scrape", function(req, res) {
           console.log(dbArticle);
         })
         .catch(function(err) {
-          // If an error occurred, log it
           console.log(err);
         });
     });
 
-    // Send a message to the client
     res.send("Scrape Complete");
 
   });
@@ -103,35 +97,29 @@ app.get("/articles", function(req, res) {
       let allArticles = {
         articles: dbArticle
       };
-      // If we were able to successfully find Articles, send them back to the client
       // res.json(dbArticle);
       res.render("index", allArticles);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
+
       res.json(err);
     });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
     .populate("comment")
     .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
   db.Comment.create(req.body)
     .then(function(dbNote) {
 
@@ -147,7 +135,6 @@ app.post("/articles/:id", function(req, res) {
     });
 });
 
-// Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
